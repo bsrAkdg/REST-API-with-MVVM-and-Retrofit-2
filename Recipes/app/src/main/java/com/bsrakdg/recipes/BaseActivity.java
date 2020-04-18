bac011d9a1b0697d5e696e0f0d28db97ec41e9ca
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-18 19:05:13 +0300

    BaseActivity create

---
+++ Recipes/app/src/main/java/com/bsrakdg/recipes/BaseActivity.java
@@ -0,0 +1,28 @@
+package com.bsrakdg.recipes;
+
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater()
+                .inflate(R.layout.activity_base, null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+
+        progressBar = constraintLayout.findViewById(R.id.progress_bar);
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+        super.setContentView(layoutResID);
+    }
+
+    protected void showProgressBar(boolean visibility) {
+        progressBar.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
+    }
+}
--- Recipes/app/src/main/java/com/bsrakdg/recipes/RecipeListActivity.java
+++ Recipes/app/src/main/java/com/bsrakdg/recipes/RecipeListActivity.java
@@ -1,14 +1,11 @@
 package com.bsrakdg.recipes;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RecipeListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe_list);
     }
 }
