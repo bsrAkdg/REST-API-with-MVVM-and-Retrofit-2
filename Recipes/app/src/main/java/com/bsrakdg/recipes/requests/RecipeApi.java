510d3cabedca449a1262b6309f446f98200e973c
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-18 21:50:17 +0300

    Retrofit add

---
+++ Recipes/app/src/main/java/com/bsrakdg/recipes/requests/RecipeApi.java
@@ -0,0 +1,5 @@
+package com.bsrakdg.recipes.requests;
+
+public interface RecipeApi {
+
+}
---
+++ Recipes/app/src/main/java/com/bsrakdg/recipes/requests/ServiceGenerator.java
@@ -0,0 +1,20 @@
+package com.bsrakdg.recipes.requests;
+
+import com.bsrakdg.recipes.util.Constants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+    private static RecipeApi recipeApi = retrofit.create(RecipeApi.class);
+
+    public RecipeApi getRecipeApi() {
+        return recipeApi;
+    }
+}
