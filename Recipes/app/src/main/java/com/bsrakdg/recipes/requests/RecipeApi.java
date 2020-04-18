3f48281f2d9f33ddf54678b59ea93033b4a46851
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-04-18 22:24:40 +0300

    RecipeApi edit

--- Recipes/app/src/main/java/com/bsrakdg/recipes/requests/RecipeApi.java
+++ Recipes/app/src/main/java/com/bsrakdg/recipes/requests/RecipeApi.java
@@ -8,18 +8,18 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface RecipeApi {
-    // Get recipe
+    // Search
     @GET("api/get")
     Call<RecipeResponse> getRecipe(
             @Query("key") String key,
-            @Query("q") String query,
-            @Query("page") String page
+            @Query("rId") String recipe_id
     );
 
-    // Search
+    // Get recipe
     @GET("api/search")
     Call<RecipeSearchResponse> searchRecipe(
             @Query("key") String key,
-            @Query("rId") String recipe_id
+            @Query("q") String query,
+            @Query("page") String page
     );
 }
